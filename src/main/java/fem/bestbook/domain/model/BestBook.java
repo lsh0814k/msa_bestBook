cc43426280b30fcdd60b2e919ff73d711d7f314a
lee <lsh0814k@gmail.com>
2024-02-02 19:15:30 -0800

    mongo 매핑

--- src/main/java/fem/bestbook/domain/model/BestBook.java
+++ src/main/java/fem/bestbook/domain/model/BestBook.java
@@ -2,6 +2,8 @@ package fem.bestbook.domain.model;
 
 import fem.bestbook.domain.model.vo.Item;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
@@ -9,11 +11,13 @@ import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
 @Getter
+@Document
 @EqualsAndHashCode(of = "id")
 @Builder(access = PRIVATE)
 @AllArgsConstructor(access = PRIVATE)
 @NoArgsConstructor(access = PROTECTED)
 public class BestBook {
+    @Id
     private String id;
     private Item item;
     private long rentCount;
